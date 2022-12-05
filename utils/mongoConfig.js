7841e04d672530b00cb3c053e7f3a07aacc3ff91
moschidisg <mosxidismosxidis@gmail.com>
2022-12-06 00:02:51 +0200

    remove boilerplate and install basic packages

---
+++ utils/mongoConfig.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const mongoDB = process.env.MONGO_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function () {
+    ('Connected to MongoDB!');
+});
