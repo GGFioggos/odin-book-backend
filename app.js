189b910769f1ca5405f8ebfb402a5f05041391be
moschidisg <mosxidismosxidis@gmail.com>
2022-12-08 23:30:02 +0200

    remove unwanted code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+var cors = require('cors');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,13 +8,13 @@ var logger = require('morgan');
 var session = require('express-session');
 
 // DB SETUP
-require('./utils/mongoConfig');
+//require('./utils/mongoConfig');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,21 +23,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
 
 module.exports = app;
