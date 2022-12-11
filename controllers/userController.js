5c4cb324f6b56f98efd7ac31b53c614e83c7c153
moschidisg <mosxidismosxidis@gmail.com>
2022-12-11 23:00:38 +0200

    edit user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,21 +2,20 @@ const async = require('async');
 
 const User = require('../models/User');
 
-let users = [
-    {
-        id: 1,
-        firstname: 'John',
-        lastname: 'Smith',
-        email: 'john@smoth@gmail.com',
-    },
-    {
-        id: 2,
-        firstname: 'Mpampis',
-        lastname: 'sougias',
-        email: 'sougias@gmail.com',
-    },
-];
-
 exports.get_user = (req, res, next) => {
-    res.json(users[req.params.id].firstname);
+    User.findById(req.params.id)
+        .populate('posts friends friendRequests')
+        .exec(function (err, user) {
+            if (err) {
+                res.json({ error: err.message });
+                return;
+            }
+
+            if (user == null) {
+                res.json({ error: 'User not found' });
+                return;
+            }
+
+            res.json(user);
+        });
 };
