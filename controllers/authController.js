a7a4eafbf0d94ac0512d32443063e93b6f6e1b5c
moschidisg <mosxidismosxidis@gmail.com>
2022-12-18 12:56:19 +0200

    disable accounts with the same email

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,29 +2,28 @@ const { body, validationResult } = require('express-validator');
 const faker = require('faker');
 const bcrypt = require('bcryptjs');
 
+const User = require('../models/User');
+
 exports.sign_up = [
-    body('firstName')
+    body('firstName', 'First name is required')
         .trim()
         .escape()
         .notEmpty()
-        .isLength({ min: 3, max: 25 })
-        .withMessage('Name must not exceed 25 characters'),
-    body('lastName')
+        .isLength({ min: 3, max: 25 }),
+    body('lastName', 'Last name required')
         .trim()
         .escape()
         .notEmpty()
-        .isLength({ min: 3, max: 25 })
-        .withMessage('Last name must not exceed 25 characters'),
-    body('email')
+        .isLength({ min: 3, max: 25 }),
+    body('email', 'Email required')
         .trim()
         .escape()
         .isEmail()
         .withMessage('Please enter a valid email'),
-    body('password')
+    body('password', 'Password required')
         .trim()
         .escape()
-        .isLength({ min: 4, max: 25 })
-        .withMessage('Password must not exceed 25 characters'),
+        .isLength({ min: 4, max: 25 }),
     body('profilePictureUrl')
         .optional()
         .trim()
@@ -36,25 +35,38 @@ exports.sign_up = [
         if (!errors.isEmpty()) {
             return res.json({ error: errors.array() });
         }
-
-        bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        // CHECK IF USER WITH THE SAME EMAIL EXISTS
+        User.findOne({ email: req.body.email }, (err, user) => {
             if (err) {
-                return next(err);
+                return res.json(err);
             }
-            const user = new User({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: hashedPassword,
-                profilePictureUrl: req.body.profilePictureUrl
-                    ? req.body.profilePictureUrl
-                    : faker.image.imageUrl(),
-            }).save((err) => {
-                if (err) {
-                    return next(err);
-                }
-                return res.json({ message: 'Success' });
-            });
+            // USER EXISTS
+            if (user) {
+                return res.json({
+                    error: 'A user with the same email already exists',
+                });
+            } else {
+                // USER DOES NOT EXIST
+                bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    const user = new User({
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        email: req.body.email,
+                        password: hashedPassword,
+                        profilePictureUrl: req.body.profilePictureUrl
+                            ? req.body.profilePictureUrl
+                            : faker.image.imageUrl(),
+                    }).save((err) => {
+                        if (err) {
+                            return next(err);
+                        }
+                        return res.json({ message: 'Success' });
+                    });
+                });
+            }
         });
     },
 ];
