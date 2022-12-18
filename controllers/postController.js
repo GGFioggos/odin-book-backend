20b19ac24bcaf43d1e8aa4bf02cad41909e39161
moschidisg <mosxidismosxidis@gmail.com>
2022-12-18 13:58:50 +0200

    add post creation

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,36 @@
 const async = require('async');
+const { body, validationResult } = require('express-validator');
 
 const Post = require('../models/Post');
+const User = require('../models/User');
 
-exports.get_post = (req, res, next) => {};
+exports.create_post = [
+    body('content', 'Post content is required').trim().escape(),
+    (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.json(errors);
+        }
+
+        User.findOne({ email: req.user.email }, (err, user) => {
+            if (err) {
+                return res.json(err);
+            }
+
+            const post = new Post({
+                author: user,
+                content: req.body.content,
+                timestamp: Date.now(),
+                comments: [],
+                likes: [],
+            }).save((err) => {
+                if (err) {
+                    return res.json(err);
+                }
+
+                return res.json({ message: 'Post created!' });
+            });
+        });
+    },
+];
--- routes/post.js
+++ routes/post.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 const postController = require('../controllers/postController');
+const { cookieJwtAuth } = require('../middleware/cookieJwtAuth');
 
 // PATH /POST
 
-router.get('/:id', postController.get_post);
+router.post('/create', cookieJwtAuth, postController.create_post);
 
 module.exports = router;
