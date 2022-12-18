0c92d2f835bf733a8ed52b51dd27dcef0244e4fa
moschidisg <mosxidismosxidis@gmail.com>
2022-12-18 13:59:17 +0200

    fix user json encapsulation

--- middleware/cookieJwtAuth.js
+++ middleware/cookieJwtAuth.js
@@ -4,7 +4,7 @@ exports.cookieJwtAuth = (req, res, next) => {
     const token = req.cookies.token;
 
     try {
-        const user = jwt.verify(token, process.env.SECRET);
+        const { user } = jwt.verify(token, process.env.SECRET);
         req.user = user;
         next();
     } catch (err) {
