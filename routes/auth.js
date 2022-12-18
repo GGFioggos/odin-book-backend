08964fd5edf7f4eaf7672d2a5133877f73a9deb9
moschidisg <mosxidismosxidis@gmail.com>
2022-12-18 13:14:23 +0200

    move log in function

--- routes/auth.js
+++ routes/auth.js
@@ -9,38 +9,7 @@ const User = require('../models/User');
 
 // PATH /AUTH
 
-router.post('/log-in', async (req, res) => {
-    const { email, password } = req.body;
-
-    User.findOne({ email: email }, (err, user) => {
-        if (err) {
-            return next(err);
-        }
-
-        if (!user) {
-            return res.json({ error: 'User does not exist' });
-        }
-
-        bcrypt.compare(password, user.password, function (err, results) {
-            if (err) {
-                throw new Error(err);
-            }
-
-            if (results) {
-                const token = jwt.sign({ user }, process.env.SECRET, {
-                    expiresIn: '10m',
-                });
-
-                res.cookie('token', token, { httpOnly: true });
-                return res.json({
-                    message: 'Log in success',
-                });
-            } else {
-                return res.json({ error: 'Passwords do not match' });
-            }
-        });
-    });
-});
+router.post('/log-in', authController.log_in);
 
 router.post('/sign-up', authController.sign_up);
 
