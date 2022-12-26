36f0a11efcf35ed7fd8fdfb832dd7a1d3c4aee07
moschidisg <mosxidismosxidis@gmail.com>
2022-12-26 13:49:45 +0200

    change user password sharing

--- controllers/authController.js
+++ controllers/authController.js
@@ -75,34 +75,35 @@ exports.sign_up = [
 exports.log_in = async (req, res) => {
     const { email, password } = req.body;
 
-    User.findOne({ email: email }, (err, user) => {
-        if (err) {
-            return next(err);
-        }
-
-        if (!user) {
-            return res.json({ error: 'User does not exist' });
-        }
-
-        bcrypt.compare(password, user.password, function (err, results) {
+    User.findOne({ email: email })
+        .select('+password')
+        .exec(function (err, user) {
             if (err) {
-                throw new Error(err);
+                return next(err);
             }
 
-            if (results) {
-                const token = jwt.sign({ user }, process.env.SECRET, {
-                    expiresIn: '30m',
-                });
-
-                res.cookie('token', token, { httpOnly: true });
-                return res.json({
-                    message: 'Log in success',
-                });
-            } else {
-                return res.json({ error: 'Passwords do not match' });
+            if (!user) {
+                return res.json({ error: 'User does not exist' });
             }
+
+            bcrypt.compare(password, user.password, function (err, results) {
+                if (err) {
+                    throw new Error(err);
+                }
+
+                if (results) {
+                    const token = jwt.sign({ user }, process.env.SECRET, {
+                        expiresIn: '30m',
+                    });
+
+                    res.cookie('token', token, { httpOnly: true });
+                    return res.json({
+                        message: 'Log in success',
+                    });
+                } else {
+                    return res.json({ error: 'Passwords do not match' });
+                }
+            });
         });
-    });
 };
-
 exports.log_out = (req, res) => {};
--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String },
+    password: { type: String, select: false },
     facebookId: { type: String },
     profilePictureUrl: { type: String },
     posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
