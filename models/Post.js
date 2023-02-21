c26650408bff5eedd280504193d9cab93d970f0e
moschidisg <mosxidismosxidis@gmail.com>
2023-02-21 19:34:08 +0200

    add time diff virtual and options to include virtuals in api

--- models/Post.js
+++ models/Post.js
@@ -1,18 +1,43 @@
 const mongoose = require('mongoose');
 var { format } = require('date-fns');
+const months = require('../utils/datehelper');
 
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-    timestamp: { type: Date, required: true },
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-});
+const opts = { toJSON: { virtuals: true } };
+
+const PostSchema = new Schema(
+    {
+        author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        content: { type: String, required: true },
+        timestamp: { type: Date, required: true },
+        comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+        likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    },
+    opts
+);
 
 PostSchema.virtual('formatted_time').get(function () {
     return format(new Date(this.timestamp), "dd MMMM yyyy ' at ' HH:mm");
+});
+
+PostSchema.virtual('time_diff').get(function () {
+    const diffInMilliseconds = Math.abs(this.timestamp - Date.now());
+    const diffInHours = Math.ceil(diffInMilliseconds / (1000 * 60 * 60));
+    if (diffInHours < 24) {
+        return diffInHours + 'Hours ago';
+    } else if (diffInHours < 168) {
+        return Math.floor(diffInHours / 24) + ' Days ago';
+    } else {
+        const date = new Date(this.timestamp);
+        return (
+            date.getDate() +
+            ' ' +
+            months[date.getMonth()] +
+            ' ' +
+            date.getFullYear()
+        );
+    }
 });
 
 module.exports = mongoose.model('Post', PostSchema);
