d28df4152d4884e101f4be79befa41184352760d
moschidisg <mosxidismosxidis@gmail.com>
2023-03-30 00:18:54 +0300

    cyclic fix

--- utils/mongoConfig.js
+++ utils/mongoConfig.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
-async function dbConnect() {
+const dbConnect = async () => {
     const mongoDB = process.env.MONGO_URI;
-    await mongoose.connect(mongoDB, {
+
+    const db = await mongoose.connect(mongoDB, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
-    const db = await mongoose.connection;
-    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-    db.once('open', function () {
-        ('Connected to MongoDB!');
-    });
-}
+};
 
 module.exports = { dbConnect };
